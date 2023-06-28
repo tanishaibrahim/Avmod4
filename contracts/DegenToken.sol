09e53349d718970c99eb8a3c5a58c16440127ebd
Tanisha Ibrahim <131354740+tanishaibrahim@users.noreply.github.com>
2023-06-28 23:29:08 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -2,13 +2,25 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
+    string public items;
+    constructor() ERC20("Degen", "DGN") {
+        items="Items available in the shop are 1. Degen Stickers 2. Degen Bottle 3. Degen printed T-shirt";
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    function mint(address to_address, uint256 amt) public onlyOwner {
+        _mint(to_address, amt);
+        
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function redeem(uint256 redeem_amount) public{
+        
+        require(balanceOf(msg.sender)>0, "Mint more tokens!! Atleast more than 1");
+        require(redeem_amount<4 , "Unfortunately, we have only three items in our shop");
+        _burn(msg.sender, redeem_amount*1);
+       
     }
 }
